b4fa1712ce96b682130ac52b26e0d281abdb9da9
Adam Tsai <adam780930@gmail.com>
2025-11-06 22:50:22 -0800

    updated contact me form styling using bootstrap

--- src/components/ContactmeForm.js
+++ src/components/ContactmeForm.js
@@ -16,27 +16,54 @@ function ContactmeForm() {
 
   return (
     <section>
-      <header className="mt-5 text-center">
-        <span className="h1 px-2 py-2 border border-dark rounded bg-light">Contact me</span>
-      </header>
-      <form className="h1 w-50 mx-auto mt-5 px-2 py-2 border border-dark rounded bg-light" onSubmit={Emailsubmit}>
-        <div className="">
+      <form
+        className="w-50 mx-auto mt-5 px-2 py-2 border border-dark rounded bg-light"
+        onSubmit={Emailsubmit}
+      >
+        <header className="my-2 text-center">
+          <span className="h1">Contact me</span>
+        </header>
+        <div className="form-group">
           <label htmlFor="name">Your Name</label>
-          <input type="text" required id="name" name="name" />
+          <input
+            type="text"
+            required
+            id="name"
+            name="name"
+            className="form-control"
+          />
         </div>
-        <div className="">
+        <div className="form-group">
           <label htmlFor="email">Your Email</label>
-          <input type="email" required id="email" name="email" />
+          <input
+            type="email"
+            required
+            id="email"
+            name="email"
+            className="form-control"
+          />
         </div>
-        <div className="">
+        <div className="form-group">
           <label htmlFor="subject">Subject</label>
-          <input type="text" required id="subject" name="subject" />
+          <input
+            type="text"
+            required
+            id="subject"
+            name="subject"
+            className="form-control"
+          />
         </div>
-        <div className="">
+        <div className="form-group">
           <label htmlFor="message">Message</label>
-          <textarea id="message" required row="5" name="message"></textarea>
+          <textarea
+            id="message"
+            required
+            row="5"
+            name="message"
+            className="form-control"
+          ></textarea>
         </div>
-        <div className="">
+        <div className="d-grid gap-2 d-md-flex justify-content-md-center">
           <button>Send Message</button>
         </div>
       </form>
